697753de1b7201fdead591e1598ecb4d298d6a7a
スカイ | skye <biswanathdas8307@gmail.com>
2026-07-07 13:02:08 +0530

    Update API base URL in app.js

--- docs/app.js
+++ docs/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const API_BASE = "https://saavn.sumit.co/api/";
+  const API_BASE = "https://saavn.dev/api";
   const LOGO_URL = "https://raw.githubusercontent.com/pawjects/Pawtify/refs/heads/main/assets/pawtify.png";
 
   const STORAGE = {
